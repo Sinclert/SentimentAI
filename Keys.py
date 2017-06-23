a3f9f0e6c136e7ba313e822c600125c4f6c1cdc7
Sinclert <Sinclert@hotmail.com>
2017-06-22 17:05:23 -0700

    Uploading Keys file

---
+++ Keys.py
@@ -0,0 +1,8 @@
+# Created by Sinclert Perez (Sinclert@hotmail.com)
+
+keys = dict(
+    consumer_key = "RgPpcHKFs2Pxknikses4WX6po",
+    consumer_secret = "JyPkucR4bKGvIcPmazxtAoVHvbPkxCRgekTKpaGfL3iO3i5nlq",
+    access_token = "1545921235-PkcGTpBadUXl4oUAoQT72O1Spm5RTUmn3aKUcoo",
+    access_token_secret = "CyaYfakHwGGb0SVcUaiOwVUnuXfQkwUHxMLDcBSXmF5m1"
+)
