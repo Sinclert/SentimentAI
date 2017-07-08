e184166dbf5198f73530a0de1434d9d2b9e9690c
Sinclert <Sinclert@hotmail.com>
2017-07-08 17:56:21 +0200

    New Parser.py file, using 'argparse' library

--- Parser.py
+++ Parser.py
@@ -1,171 +1,241 @@
 # Created by Sinclert Perez (Sinclert@hotmail.com)
 
-import sys, os
+import os
 from Classifier import Classifier, possible_classifiers
 from DataMiner import DataMiner
 from TwitterListener import TwitterListener
 from Utilities import getSentences
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 
 # Folder paths
 datasets_folder = "Datasets"
 models_folder = "Models"
 
+functions = ['classify', 'search', 'stream', 'train']
 labels = ['Negative', 'Neutral', 'Positive']
 
 
 
 
-################# TRAIN TEST #################
-# Arguments: "Train" <Classifier> <L1 file> <L2 file> <Words pct> <Bigrams pct> <Output>
+""" Trains and stores the specified ML algorithm after been trained """
+def train(classifier_name, l1_file, l2_file, words_pct, bigrams_pct, output):
 
-if (len(sys.argv) == 8) and (sys.argv[1].lower() == "train"):
+	# Checks the validity of the percentages
+	if (words_pct < 0) or (bigrams_pct < 0):
+		print("ERROR: The specified percentages must be positive")
+		exit()
 
-    # Checks the specified classifier
-    if sys.argv[2].lower() in possible_classifiers.keys():
+	# Checks the specified classifier
+	if classifier_name.lower() in possible_classifiers.keys():
 
-        l1_file = os.path.join(datasets_folder, sys.argv[3])
-        l2_file = os.path.join(datasets_folder, sys.argv[4])
-        words_pct, bigrams_pct = 0, 0
+		l1_file = os.path.join(datasets_folder, l1_file)
+		l2_file = os.path.join(datasets_folder, l2_file)
 
-        # Converting the inputs into real numeric variables
-        try:
-            words_pct = int(sys.argv[5])
-            bigrams_pct = int(sys.argv[6])
+		classifier = Classifier()
+		classifier.train(classifier_name = classifier_name.lower(),
+		                 l1_file = l1_file,
+		                 l2_file = l2_file,
+		                 words_pct = words_pct,
+		                 bigrams_pct = bigrams_pct)
 
-        except ValueError:
-            print("ERROR: The specified percentages must be integers")
-            exit()
+		classifier.saveModel(models_folder, output)
 
-        if (words_pct < 0) or (bigrams_pct < 0):
-            print("ERROR: The specified percentages must be positive")
-            exit()
-
-        classifier = Classifier()
-        classifier.train(classifier_name = sys.argv[2].lower(),
-                         l1_file = l1_file,
-                         l2_file = l2_file,
-                         words_pct = words_pct,
-                         bigrams_pct = bigrams_pct)
-
-        classifier.saveModel(models_folder, sys.argv[7])
-
-    else:
-        print("ERROR: Invalid classifier")
-        exit()
+	else:
+		print("ERROR: Invalid classifier")
+		exit()
 
 
 
 
-################# CLASSIFY TEST #################
-# Arguments: "Classify" <Classifier 1> <Classifier 2> <Twitter account> <Word>
+""" Performs sentiment analysis over the specified Twitter account """
+def classify(polarity_cls, sentiment_cls, account, filter_word):
 
-elif (len(sys.argv) == 6) and (sys.argv[1].lower() == "classify"):
+	# Loading both trained classifiers
+	classifier1 = Classifier()
+	classifier2 = Classifier()
+	classifier1.loadModel(models_folder, polarity_cls)
+	classifier2.loadModel(models_folder, sentiment_cls)
 
-    # Loading both trained classifiers
-    classifier1 = Classifier()
-    classifier2 = Classifier()
-    classifier1.loadModel(models_folder, sys.argv[2])
-    classifier2.loadModel(models_folder, sys.argv[3])
+	# Obtaining tweets
+	miner = DataMiner()
+	tweets = miner.getUserTweets(user = account,
+	                             word = filter_word)
 
-    # Obtaining tweets
-    miner = DataMiner()
-    tweets = miner.getUserTweets(user = sys.argv[4],
-                                 word = sys.argv[5])
+	# Splitting the tweets into individual sentences
+	sentences = getSentences(tweets = tweets,
+	                         word = filter_word)
 
-    # Splitting the tweets into individual sentences
-    sentences = getSentences(tweets = tweets,
-                             word = sys.argv[5])
+	# Creating the results dictionary
+	results = dict.fromkeys(labels, 0)
 
-    # Creating the results dictionary
-    results = dict.fromkeys(labels, 0)
+	# Storing the classification results
+	for sentence in sentences:
+		label = classifier1.classify(sentence)
 
-    # Storing the classification results
-    for sentence in sentences:
-        label = classifier1.classify(sentence)
+		if label == 'Polarized':
+			try:
+				results[classifier2.classify(sentence)] += 1
+			except TypeError:
+				print("Tweet:", sentence)
+				print("Ignored (features lack of information)\n")
 
-        if label == 'Polarized':
-            try:
-                results[classifier2.classify(sentence)] += 1
-            except TypeError:
-                print("Tweet:", sentence)
-                print("Ignored (features lack of information)\n")
+		elif label == 'Neutral':
+			results['Neutral'] += 1
 
-        elif label == 'Neutral':
-            results['Neutral'] += 1
-
-    print(results)
+	print(results)
 
 
 
 
-################## SEARCH TEST ##################
-# Arguments: "Search" <Search query> <Language> <Search depth> <Output>
+""" Search tweets using Twitter API storing them into the output file """
+def search(search_query, language, search_depth, output):
 
-elif (len(sys.argv) == 6) and (sys.argv[1].lower() == "search"):
+	# Creating the storing output final path
+	output_path = os.path.join(datasets_folder, output)
 
-    # Creating the storing output final path
-    output_path = os.path.join(datasets_folder, sys.argv[5])
-
-    # Obtaining tweets and storing them in a file
-    miner = DataMiner()
-    miner.searchTweets(query = sys.argv[2],
-                       language = sys.argv[3],
-                       file = output_path,
-                       depth = int(sys.argv[4]))
+	# Obtaining tweets and storing them in a file
+	miner = DataMiner()
+	miner.searchTweets(query = search_query,
+	                   language = language,
+	                   file = output_path,
+	                   depth = search_depth)
 
 
 
 
-################## STREAM TEST ##################
-# Arguments: "Stream" <Classifier 1> <Classifier 2> <Buffer size> <Query> <Language> <Coordinates>
+""" Performs sentiment analysis over a stream of tweets filter by location """
+def stream(polarity_cls, sentiment_cls, buffer_size, query, language, coordinates):
 
-elif (len(sys.argv) == 8) and (sys.argv[1].lower() == "stream"):
+	# Loading both trained classifiers
+	classifier1 = Classifier()
+	classifier2 = Classifier()
+	classifier1.loadModel(models_folder, polarity_cls)
+	classifier2.loadModel(models_folder, sentiment_cls)
 
-    # Loading both trained classifiers
-    classifier1 = Classifier()
-    classifier2 = Classifier()
-    classifier1.loadModel(models_folder, sys.argv[2])
-    classifier2.loadModel(models_folder, sys.argv[3])
+	# Parsing coordinates
+	coordinates = coordinates.split(',')
 
-    # Parsing arguments
-    buffer_size = int(sys.argv[4])
-    tracks = sys.argv[5].split(',')
-    languages = sys.argv[6].split(',')
-    coordinates = sys.argv[7].split(',')
+	if len(coordinates) % 4 != 0:
+		print("ERROR: The number of coordinates must be a multiple of 4")
+		exit()
 
-    if len(coordinates) % 4 != 0:
-        print("ERROR: The number of coordinates must be a multiple of 4")
-        exit()
+	coordinates = [float(coord) for coord in coordinates]
 
-    coordinates = [float(coord) for coord in coordinates]
+	# Creates the stream object and start stream
+	listener = TwitterListener(classifier1, classifier2, buffer_size, labels)
+	listener.initStream(query, language, coordinates)
 
+	from matplotlib import pyplot, animation
+	from GraphAnimator import animatePieChart, figure
 
-    # Creates the stream object and start stream
-    listener = TwitterListener(classifier1, classifier2, buffer_size, labels)
-    listener.initStream(tracks, languages, coordinates)
+	# Animate the graph each milliseconds interval
+	animation.FuncAnimation(fig = figure,
+	                        func = animatePieChart,
+	                        interval = 500,
+	                        fargs = (labels, query, listener.stream_dict))
+	pyplot.show()
 
-
-    from matplotlib import pyplot, animation
-    from GraphAnimator import animatePieChart, figure
-
-    # Animate the graph each milliseconds interval
-    ani = animation.FuncAnimation(fig = figure,
-                                  func = animatePieChart,
-                                  interval = 500,
-                                  fargs = (labels, tracks, listener.stream_dict))
-    pyplot.show()
-
-    # Finally: close the stream process
-    listener.closeStream()
+	# Finally: close the stream process
+	listener.closeStream()
 
 
 
 
-# In case none of the possible options is selected: error
-else:
-    print("ERROR: Invalid arguments. Possible options:")
-    print("1. 'Train' <Classifier> <L1 file> <L2 file> <Words pct> <Bigrams pct> <Output>")
-    print("2. 'Classify' <Classifier 1> <Classifier 2> <Twitter account> <Word>")
-    print("3. 'Search' <Search query> <Language> <Search depth> <Output>")
-    print("4. 'Stream' <Classifier 1> <Classifier 2> <Buffer size> <Query> <Language> <Coordinates>")
+""" Main method that parses the arguments and call the proper function """
+if __name__ == '__main__':
+
+	# Creating the top-level parser
+	global_parser = ArgumentParser(
+		usage = "Parser.py [mode] [arguments]",
+		description = "modes and arguments:\n"
+		              "  \n"
+		              "  classify: analyses tweets of a Twitter account\n"
+		              "            -p <polarity classifier>\n"
+		              "            -s <sentiment classifier>\n"
+		              "            -a <Twitter account>\n"
+		              "            -w <filter word>\n"
+		              "  \n"
+		              "  search: stores query tweets into a new dataset\n"
+		              "            -q <query>\n"
+		              "            -l <language>\n"
+		              "            -d <search depth>\n"
+		              "            -o <dataset output>\n"
+		              "  \n"
+		              "  stream: analyses tweets of a Twitter stream\n"
+		              "            -p <polarity classifier>\n"
+		              "            -s <sentiment classifier>\n"
+		              "            -b <buffer size>\n"
+		              "            -w <filter word>\n"
+		              "            -l <language>\n"
+		              "            -c <coordinates>\n"
+		              "  \n"
+		              "  train: trains and store the specified ML model\n"
+		              "            -n <classifier name>\n"
+		              "            -f1 <first dataset>\n"
+		              "            -f2 <second dataset>\n"
+		              "            -w <words percentage as features>\n"
+		              "            -b <bigrams percentage as features>\n"
+		              "            -o <output name>\n",
+		formatter_class = RawDescriptionHelpFormatter)
+
+
+	# Parsing the arguments in order to check the mode
+	global_parser.add_argument('mode', choices = functions)
+	arg, func_args = global_parser.parse_known_args()
+
+
+	# First mode: classify
+	if arg.mode == "classify":
+
+		classify_par = ArgumentParser(usage = "Use 'Parser.py -h' for help")
+		classify_par.add_argument('-p', required = True)
+		classify_par.add_argument('-s', required = True)
+		classify_par.add_argument('-a', required = True)
+		classify_par.add_argument('-w', required = True)
+
+		args = classify_par.parse_args(func_args)
+		classify(args.p, args.s, args.a, args.w)
+
+
+	# Second mode: search
+	elif arg.mode == "search":
+
+		search_par = ArgumentParser(usage = "Use 'Parser.py -h' for help")
+		search_par.add_argument('-q', required = True)
+		search_par.add_argument('-l', required = True)
+		search_par.add_argument('-d', required = True, type = int)
+		search_par.add_argument('-o', required = True)
+
+		args = search_par.parse_args(func_args)
+		search(args.q, args.l, args.d, args.o)
+
+
+	# Third mode: stream
+	elif arg.mode == "stream":
+
+		stream_par = ArgumentParser(usage = "Use 'Parser.py -h' for help")
+		stream_par.add_argument('-p', required = True)
+		stream_par.add_argument('-s', required = True)
+		stream_par.add_argument('-b', required = True, type = int)
+		stream_par.add_argument('-q', required = True)
+		stream_par.add_argument('-l', required = True)
+		stream_par.add_argument('-c', required = True)
+
+		args = stream_par.parse_args(func_args)
+		stream(args.p, args.s, args.b, args.q, args.l, args.c)
+
+
+	# Fourth mode: train
+	elif arg.mode == "train":
+
+		train_par = ArgumentParser(usage = "Use 'Parser.py -h' for help")
+		train_par.add_argument('-n', required = True)
+		train_par.add_argument('-f1', required = True)
+		train_par.add_argument('-f2', required = True)
+		train_par.add_argument('-w', required = True, type = int)
+		train_par.add_argument('-b', required = True, type = int)
+		train_par.add_argument('-o')
+
+		args = train_par.parse_args(func_args)
+		train(args.n, args.f1, args.f2, args.w, args.b, args.o)
