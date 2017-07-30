6727e92be5497bafbce03a2f65701202209178ec
Sinclert <Sinclert@hotmail.com>
2017-07-30 18:02:44 +0200

    New key values

--- Keys.py
+++ Keys.py
@@ -3,6 +3,6 @@
 keys = dict(
     consumer_key = "RgPpcHKFs2Pxknikses4WX6po",
     consumer_secret = "JyPkucR4bKGvIcPmazxtAoVHvbPkxCRgekTKpaGfL3iO3i5nlq",
-    access_token = "1545921235-PkcGTpBadUXl4oUAoQT72O1Spm5RTUmn3aKUcoo",
-    access_token_secret = "CyaYfakHwGGb0SVcUaiOwVUnuXfQkwUHxMLDcBSXmF5m1"
+    access_token = "1545921235-AZYG5NmPaVpWfZNJOM2qnLbmlN00sUKUuolPBkl",
+    access_token_secret = "98ovMGWbkrk19ggrzvP8l4bqY48bEtw3S3j5r7BR1G1vG"
 )
