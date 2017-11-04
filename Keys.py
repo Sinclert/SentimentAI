7f564ab831a762f9a14522064ee0845eedf9414f
Sinclert <Sinclert@hotmail.com>
2017-11-04 18:29:18 +0100

    Changing Keys
    Good luck trying to use the previous ones :)

--- Keys.py
+++ Keys.py
@@ -1,8 +1,8 @@
 # Created by Sinclert Perez (Sinclert@hotmail.com)
 
 keys = dict(
-    consumer_key = "RgPpcHKFs2Pxknikses4WX6po",
-    consumer_secret = "JyPkucR4bKGvIcPmazxtAoVHvbPkxCRgekTKpaGfL3iO3i5nlq",
-    access_token = "1545921235-AZYG5NmPaVpWfZNJOM2qnLbmlN00sUKUuolPBkl",
-    access_token_secret = "98ovMGWbkrk19ggrzvP8l4bqY48bEtw3S3j5r7BR1G1vG"
+    consumer_key = "<INTRODUCE YOUR OWN KEYS>",
+    consumer_secret = "<INTRODUCE YOUR OWN KEYS>",
+    access_token = "<INTRODUCE YOUR OWN KEYS>",
+    access_token_secret = "<INTRODUCE YOUR OWN KEYS>"
 )
