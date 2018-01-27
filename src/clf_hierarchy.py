a795e232fe5b7b4efe44d0adca6de4d8e2aba72a
Sinclert <Sinclert@hotmail.com>
2018-01-27 16:04:59 +0100

    Pythonic clf_hierarchy

--- src/clf_hierarchy.py
+++ src/clf_hierarchy.py
@@ -1,51 +1,106 @@
 # Created by Sinclert Perez (Sinclert@hotmail.com)
 
-from clf_model import Classifier
-from utils import getFileJSON
+
+from clf_node import NodeClassif
+from utils import check_keys
+from utils import get_file_json
 
 
-""" Class in charge of the hierarchical classification of sentences """
-class Hierarchical_cls(object):
+class HierarchicalClassif(object):
+
+	""" Represents a hierarchical classification tree
+
+	Attributes
+	----------
+	tree : dictionary
+		represents a tree structure in which each node has:
+
+		- clf_name : string
+		- clf_object : classifier object
+		- clf_children : dict
+
+	keys : list
+		required keys in each JSON tree node
+	"""
 
 
-	""" Initiates variables when the instance is created """
-	def __init__(self, clf_profile_path):
-
-		self.clf_tree = getFileJSON(clf_profile_path)
-		self.__load_clf(node = self.clf_tree)
+	# Class attribute containing the required JSON nodes keys
+	keys = ['clf_name', 'clf_object', 'clf_children']
 
 
 
 
+	def __init__(self, profile_path):
 
-	""" Recursively loading classifier objects into dictionary keys """
+		""" Loads the JSON profile models into the tree attribute
+
+		Arguments
+		---------
+		profile_path : string
+			relative path to the JSON file containing the classifiers info
+		"""
+
+		profile = get_file_json(profile_path)
+
+		self.tree = profile['tree']
+		self.__load_clf(self.tree)
+
+
+
+
 	def __load_clf(self, node):
 
-		clf_node = Classifier()
-		clf_node.loadModel("models", node['clf_name'])
-		node['clf_object'] = clf_node
+		""" Recursively check and load classifier objects into 'clf_object'
+
+		Arguments
+		---------
+		node : dict
+			current tree node to load the classifier specified by 'clf_name'
+		"""
+
+		if not check_keys(keys = self.keys, dict = node):
+			exit('Invalid JSON keys')
+
+		node['clf_object'] = NodeClassif(node['clf_name'])
 
 		try:
-			for children in node['clf_children'].values():
-				self.__load_clf(node = children)
-		except KeyError:
-			pass
+			clf_labels = node['clf_object'].get_labels()
+			clf_children_names = node['clf_children'].keys()
+			clf_children_nodes = node['clf_children'].values()
+
+			if not check_keys(keys = clf_children_names, dict = clf_labels):
+				exit('Invalid JSON children names')
+
+			for child_node in clf_children_nodes:
+				self.__load_clf(child_node)
+
+		except AttributeError:
+			exit('Invalid JSON values')
 
 
 
 
-	""" Predicts the final label of a sentence """
 	def predict(self, sentence):
 
-		node = self.clf_tree
-		label = node['clf_object'].classify(sentence)
+		""" Predicts the label of a sentence using the loaded classifiers
 
-		# Keep traversing the tree until reaching a leaf node
+		Arguments
+		---------
+		sentence : string
+			text to classify
+
+		Returns
+		---------
+		label : string / None
+			predicted sentence label. None if it could not be classified
+		"""
+
+		node = self.tree
+		label = node['clf_object'].predict(sentence)
+
 		while label in node['clf_children'].keys():
 			node = node['clf_children'][label]
-			label = node['clf_object'].classify(sentence)
+			label = node['clf_object'].predict(sentence)
 
-		if label is None:
-			print(sentence, "(Tweet ignored)")
-
+		if label is None: print(sentence, "(Unknown label)")
 		return label
